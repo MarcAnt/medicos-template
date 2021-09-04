2c32f415e3b21e0a1f298da5408f63f0dd5b70a4
MarcAnt <marc_esq_verb@hotmail.com>
2021-09-04 00:25:27 -0400

    Adding the main control theme

---
+++ src/resources/theme.js
@@ -0,0 +1,42 @@
+import { extendTheme } from "@chakra-ui/react";
+const styles = {
+  global: {
+    "html, body": {
+      bg: "#F3F6FA",
+    },
+  },
+};
+
+const colors = {
+  brand: {
+    900: "#0043A5",
+  },
+  switchBg: {
+    900: "#87878B",
+  },
+};
+
+const Button = {
+  variants: {
+    buttonPanel: {
+      color: "white",
+      justifyContent: "flex-start",
+      backgroundColor: "transparent",
+      borderTopLeftRadius: "0",
+      borderBottomLeftRadius: "0",
+      transition: "all .1s",
+      _hover: {
+        backgroundColor: "#0453C6",
+        borderLeftColor: "#96DD43",
+        borderLeftWidth: "3px",
+      },
+    },
+  },
+};
+
+const fonts = {
+  heading: "Pt Sans, sans-serif",
+  body: "Pt Sans, sans-serif",
+};
+
+export default extendTheme({ styles, colors, fonts, components: { Button } });
